d19b62af857a1461183213e1edc36eb0f2cab34e
Denilson2619 <p.denilson@escolar.ifrn.edu.br>
2025-01-12 01:16:20 -0300

    Primeiro Commit

---
+++ projeto_final/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.viewsets import ModelViewSet
+from .models import User  
+from .serializers import UserSerializer 
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+def home(request):
+    return render(request, 'index.html')
